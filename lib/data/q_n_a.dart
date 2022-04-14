55b34d148656e2728c1dbe64d48b0f9aa75900c6
Simon Beirouti <hello@simonbeirouti.com>
2022-04-14 19:46:55 +1000

    load test questions

---
+++ lib/data/q_n_a.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_flashcards/models/q_n_a.dart';
+
+List<QuestionAnswer> statements = [
+  QuestionAnswer(
+    question: "What is Flutter?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is real?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is fake?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is confusing?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is forward thinking and a genius?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is fake?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is confusing?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is forward thinking and a genius?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is fake?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is confusing?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+  QuestionAnswer(
+    question: "What is forward thinking and a genius?",
+    answer: "Flutter is a framework for cross platform applications!",
+  ),
+];
